07eba5013d7a89791f924322d451c46f733d91e9
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2016-01-02 16:57:13 -0500

    initial

---
+++ config/trailpack.js
@@ -0,0 +1,14 @@
+/**
+ * Trailpack Configuration
+ *
+ * This manifest declares the application resources which are provided and/or
+ * modified by this trailpack.
+ */
+module.exports = {
+
+  /**
+   * Define the API resources are provided by this trailpack
+   */
+  provides: { },
+
+}
